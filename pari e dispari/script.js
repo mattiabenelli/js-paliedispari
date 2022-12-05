5136a6e1e0c6dc816316c29c47c451a9e7762a2f
mattiabenelli <benellimattia0@gmail.com>
2022-12-05 17:13:19 +0100

    pari dispari done

--- pari e dispari/script.js
+++ pari e dispari/script.js
@@ -1,14 +1,34 @@
 function generatorRandomNumbers(min, max){
     let randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
-    console.log(randomNumber)
     return randomNumber
 }
 
 let disparipari 
 do{
     disparipari = prompt("scegli pari o dispari: ");
-        if(disparipari != 'pari' || disparipari != 'dispari'){
+        if(disparipari !== 'pari' && disparipari !== 'dispari'){
             alert("hai inserito un valore non valido")
         }    
 }
-while(disparipari !== 'pari' || disparipari !== 'dispari');
+while(disparipari != 'pari' && disparipari != 'dispari');
+
+let primoNumero = generatorRandomNumbers(1, 5);
+let secondoNumero = generatorRandomNumbers(1, 5);
+console.log(`il primo numero è: ${primoNumero}`);
+console.log(`il secondo numero è: ${secondoNumero}`);
+
+let somma = primoNumero + secondoNumero;
+console.log(`la loro somma è: ${somma}`);
+
+if( somma % 2 == 0 && disparipari == 'pari'){
+    alert("hai vinto");
+}
+else if(somma % 2 == 0 && disparipari == 'dispari'){
+    alert("hai perso");
+}
+else if(somma % 2 != 0 && disparipari == 'dispari'){
+    alert("hai vinto");
+}
+else{
+    alert("hai perso")
+}
